cbcd3b9d142eb26c9bc8dcd3c7239ad53c00032c
Stephanie Wang <swang93@mit.edu>
2016-09-17 15:15:18 -0700

    Make it possible to read and write data that is not null-terminated

--- test/io_tests.c
+++ test/io_tests.c
@@ -2,8 +2,9 @@
 
 #include <assert.h>
 #include <unistd.h>
+#include <inttypes.h>
 
-#include "sockets.h"
+#include "io.h"
 
 SUITE(socket_tests);
 
@@ -13,21 +14,27 @@ TEST ipc_socket_test(void) {
   ASSERT(socket_fd >= 0);
 
   char* test_string = "hello world";
+  char* test_bytes = "another string";
   pid_t pid = fork();
   if (pid == 0) {
     close(socket_fd);
     socket_fd = connect_ipc_sock(socket_pathname);
     ASSERT(socket_fd >= 0);
-    send_ipc_sock(socket_fd, test_string);
+    write_string(socket_fd, test_string);
+    write_bytes(socket_fd, (uint8_t*) test_bytes, strlen(test_bytes));
     close(socket_fd);
     exit(0);
   } else {
     int client_fd = accept_client(socket_fd);
     ASSERT(client_fd >= 0);
-    char* message = recv_ipc_sock(client_fd);
+    char* message = read_string(client_fd);
     ASSERT(message != NULL);
     ASSERT_STR_EQ(test_string, message);
     free(message);
+    int64_t len;
+    uint8_t *bytes;
+    read_bytes(client_fd, &bytes, &len);
+    ASSERT(memcmp(test_bytes, bytes, len) == 0);
     close(client_fd);
     close(socket_fd);
     unlink(socket_pathname);
