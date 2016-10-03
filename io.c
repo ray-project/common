ac8b4355989051923208e35cb1af6cffc908ec43
Stephanie Wang <wang.stephanie93@gmail.com>
2016-10-03 14:34:59 -0700

    Socket methods that block until full message is read/written

--- io.c
+++ io.c
@@ -95,6 +95,24 @@ int accept_client(int socket_fd) {
 }
 
 /**
+ * Reliably write a sequence of bytes into a file descriptor. This will block
+ * until one of the following happens: (1) there is an error (2) end of file,
+ * or (3) all length bytes have been written.
+ */
+void write_reliable(int fd, char *cursor, size_t length) {
+  ssize_t nbytes = 0;
+  while (length > 0) {
+    /* While we haven't written the whole message, write to the file
+     * descriptor, advance the cursor, and decrease the amount left to write.
+     * */
+    nbytes = write(fd, cursor, length);
+    CHECK(nbytes > 0);
+    cursor += nbytes;
+    length -= nbytes;
+  }
+}
+
+/**
  * Write a sequence of bytes on a file descriptor. The bytes should then be read
  * by read_message.
  *
@@ -105,17 +123,41 @@ int accept_client(int socket_fd) {
  * @return Void.
  */
 void write_message(int fd, int64_t type, int64_t length, uint8_t *bytes) {
-  ssize_t nbytes = write(fd, (char *) &type, sizeof(type));
-  CHECK(nbytes == sizeof(int64_t));
-  nbytes = write(fd, (char *) &length, sizeof(length));
-  CHECK(nbytes == sizeof(int64_t));
-  nbytes = write(fd, (char *) bytes, length * sizeof(char));
-  CHECK(nbytes >= 0);
+  write_reliable(fd, (char *) &type, sizeof(type));
+  write_reliable(fd, (char *) &length, sizeof(length));
+  write_reliable(fd, (char *) bytes, length * sizeof(char));
 }
 
 /**
- * Read a sequence of bytes written by write_bytes from a file descriptor. This
- * allocates space for the message.
+ * The read method corresponding to write_reliable. Note that the given cursor
+ * must already have allocated length number of bytes before calling this
+ * method.
+ */
+int read_reliable(int fd, char *cursor, size_t length) {
+  ssize_t nbytes = 0;
+  while (length > 0) {
+    /* While we haven't read the whole message, read from the file descriptor,
+     * advance the cursor, and decrease the amount left to read.  */
+    nbytes = read(fd, cursor, length);
+    if (nbytes < 0) {
+      if (errno == EAGAIN || errno == EWOULDBLOCK) {
+        continue;
+      }
+      /* Force an exit if there was any other type of error. */
+      CHECK(nbytes < 0);
+    }
+    if (nbytes == 0) {
+      return -1;
+    }
+    cursor += nbytes;
+    length -= nbytes;
+  }
+  return 0;
+}
+
+/**
+ * Read a sequence of bytes written by write_message from a file descriptor.
+ * This allocates space for the message.
  *
  * @note The caller must free the memory.
  *
@@ -130,20 +172,28 @@ void write_message(int fd, int64_t type, int64_t length, uint8_t *bytes) {
  * @return Void.
  */
 void read_message(int fd, int64_t *type, int64_t *length, uint8_t **bytes) {
-  ssize_t nbytes = read(fd, type, sizeof(int64_t));
-  CHECK(nbytes >= 0);
-  /* Handle the case in which the socket is closed. */
-  if (nbytes == 0) {
-    *type = DISCONNECT_CLIENT;
-    *length = 0;
-    *bytes = NULL;
-    return;
+  int closed = read_reliable(fd, (char *) type, sizeof(int64_t));
+  if (closed) {
+    goto disconnected;
   }
-  nbytes = read(fd, length, sizeof(int64_t));
-  CHECKM(nbytes == sizeof(int64_t), "Size was %zd", nbytes);
+  closed = read_reliable(fd, (char *) length, sizeof(int64_t));
+  if (closed) {
+    goto disconnected;
+  }
   *bytes = malloc(*length * sizeof(uint8_t));
-  nbytes = read(fd, *bytes, *length);
-  CHECK(nbytes >= 0);
+  closed = read_reliable(fd, (char *) *bytes, *length);
+  if (closed) {
+    free(*bytes);
+    goto disconnected;
+  }
+  return;
+
+disconnected:
+  /* Handle the case in which the socket is closed. */
+  *type = DISCONNECT_CLIENT;
+  *length = 0;
+  *bytes = NULL;
+  return;
 }
 
 /* Write a null-terminated string to a file descriptor. */
